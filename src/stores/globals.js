29be29313703160ab052a30342960b6f8d5917b2
pste <pirraste@gmail.com>
2025-07-25 22:37:37 +0200

    [fix] vue 2 vite env handler

--- src/stores/globals.js
+++ src/stores/globals.js
@@ -3,7 +3,8 @@ import { ref } from 'vue';
 
 const useGlobalsStore = defineStore('globals', () => {
     const baseFolder = ref('.');
-    const apiURL = ref(process.env.VUE_APP_APIURL);
+    const apiURL = ref(import.meta.env.VITE_APIURL);
+    console.log(apiURL)
 
     return {
         baseFolder,
