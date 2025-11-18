c788a66575ba6dee9f67653d81c8ba7092d96934
pste <pirraste@gmail.com>
2025-11-18 17:12:47 +0100

    [observer] removed from store

--- src/stores/session.js
+++ src/stores/session.js
@@ -2,7 +2,6 @@ import { defineStore } from 'pinia';
 import { inject, computed, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
-//import { useIdleObserver } from '@idle-observer/vue3'
 import usePlayerStore from '@/stores/player'
 
 //
@@ -11,53 +10,25 @@ const useSessionStore = defineStore('session', () => {
     const playerStore = usePlayerStore();
     const API = inject('API');
 
-    // idle timer
-    //let observer = buildObserver();
-    //setTimeout(observer.pause, 1000); // there's a bug in the observer, I cannot pause immediatly
-
     // data
     const user = ref({
         name: "",
         preferences: {
-            sortCollectionBy: 'artist',
+            sortCollectionBy: 'name',
             sortCollectionDirection: 'asc',
         },
     });
 
-    // methods
-    function buildObserver() {
-        const observer = useIdleObserver({
-            timeout: 8 * 1000, // secs TODO
-            idleWarningDuration: 5 * 1000, // secs
-            onIdle: () => {
-                if (playerStore.isIdle === false) {
-                    observer.reset(); // continuosly restarts if playing
-                }
-                else {
-                    userLogout();
-                }
-            },
-            onIdleWarning: function() {
-                console.log('User will be idle soon!');
-            },
-            activityEvents: ['mousemove', 'keydown'],
-        });
-        return observer;
-    }
-
     async function userLogin(name, pwd) {
         const dbuser = await API.post('/login', { username: name, password: pwd });
         user.value.name = dbuser?.username || 'anonymous';
-        //observer.resume();
     }
 
     async function userLogout() {
-        //observer.pause();
         playerStore.clear();
         user.value.name = ""; // to update loggedIn computed ASAP
 
         await API.post('/logout', { });
-        //router.replace('/');
         // heavy reload
         const { protocol, host } = window.location;
         window.location.replace(`${protocol}//${host}`);
