13e4794fab48ed95768433d3f8b479adc41e29a8
pste <pirraste@gmail.com>
2026-04-07 11:11:25 +0200

    [cover] avoid request flooding on cover requests

--- src/stores/covers.js
+++ src/stores/covers.js
@@ -5,9 +5,45 @@ import { inject } from 'vue';
 const useCoversStore = defineStore('covers', () => {
     const API = inject('API');
     const idxDB = inject('idxDB');
+    const queue = [];
+    let isProcessing = false;
+
+    async function getcover(album_id) {
+        const buffer = await API.getBlob('/search/cover', {album_id});
+        //logger.log("covers API get:", buffer);
+        await idxDB.put("covers", album_id, buffer);
+        return buffer;
+    }
+
+    async function dequeue() {
+        isProcessing = true;
+        while (queue.length >0) {
+            const {album_id,resolve,reject} = queue[0]; // take head, dont remove
+            try {
+                const buffer = await getcover(album_id); // download cover
+                resolve(buffer); // return data to the caller
+            }
+            catch(err) {
+                reject(err)
+            }
+            finally {
+                queue.shift(); // remove head
+            }
+        } // go on
+        isProcessing = false;
+    }
+
+    async function enqueue(album_id) {
+        return new Promise(async (resolve, reject) => {
+            // queued requests to avoid flooding
+            queue.push({ album_id, resolve, reject });
+            if (!isProcessing) {
+                dequeue();
+            }
+        });
+    }
 
     return {
-        // not async with intention
         get: async function(album_id) {
             const dbCover = await idxDB.get("covers", album_id);
             //logger.log("covers idxdb get:", dbCover);
@@ -17,10 +53,9 @@ const useCoversStore = defineStore('covers', () => {
             }
             // from APIs
             else {
-                const buffer = await API.getBlob('/search/cover', {album_id});
-                //logger.log("covers API get:", buffer);
-                // to local db
-                await idxDB.put("covers", album_id, buffer);
+                // queued requests to avoid flooding
+                const buffer = await enqueue(album_id);
+                // const buffer = await getcover(album_id); // request flooding ...
                 return buffer;
             }
         }
