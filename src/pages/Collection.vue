d6ea9eaaf2a580e3a3ccd201b641cde55cd8b636
pste <pirraste@gmail.com>
2026-04-10 11:26:30 +0200

    [collection] removed breakpoint every 50 items in collection

--- src/pages/Collection.vue
+++ src/pages/Collection.vue
@@ -1,40 +1,16 @@
 <script setup>
-import { inject } from 'vue'
 import { useRouter } from 'vue-router'
 import MiniDisc from '@/components/MiniDisc.vue'
 import useCollectionStore from '@/stores/collection'
-import logger from '@/plugins/logger'
 
 //
 const router = useRouter();
 const collectionStore = useCollectionStore();
 
-// sorting separators
-const separatorLetter = initLetters(); // must be NOT reactive (!) - starts from 'b'
-function initLetters() {
-    const res = [];
-    // 98 = 'b'; 122 = 'z'
-    for (let i=98; i<=122; i++) {
-        res.push(String.fromCharCode(i));
-    }
-    return res;
-}
-    
-function isFirstOfBlock(name) {
-    if (name[0] === separatorLetter[0]) {
-        separatorLetter.shift();
-        logger.log('taken ' + name)
-        return true;
-    }
-    return false;
-}
-
 // methods
 function gotoArtistAlbum(album_id) {
     router.push({ name: 'album', params: { albumid: album_id }});
 }
-
-//const testitems = ref(Array.from({ length: 120 }, (_, i) => i + 1));
 </script>
 
 <template>
@@ -48,7 +24,6 @@ function gotoArtistAlbum(album_id) {
                 @click="gotoArtistAlbum(item.album_id)"
             >
             </MiniDisc>
-            <div v-if="(index + 1) % 50 === 0" class="w-full"></div>
         </template>
     </div>
 </template>
@@ -71,14 +46,11 @@ function gotoArtistAlbum(album_id) {
         gap: 0.75rem;
     }
 }
+
 .list {
     display: inline-block;
 }
-.list.first-of-block::before {
-    content: '\A';
-    white-space: pre;
-    display: block;
-}
+
 .shadowed {
     border-radius: 10px;
     box-shadow: 3px 3px 1px 0px #8b8b92, 6px 6px 1px 0px #38383b, 9px 9px 1px 0px #000000;
