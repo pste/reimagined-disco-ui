cb2ea3fb4c5c1538a05a03f050391ab06d7a250d
pste <pirraste@gmail.com>
2026-05-26 16:29:53 +0200

    [fix] double registration of the service worker

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './style.css';
 import App from './App.vue';
 
 import { registerPlugins } from '@/plugins';
-import { registerSW } from 'virtual:pwa-register';
 
 const app = createApp(App);
 
@@ -11,12 +10,4 @@ registerPlugins(app);
 
 app.mount('#app');
 
-// PWA Service Worker Registration
-if ('serviceWorker' in navigator) {
-    import('virtual:pwa-register').then(({ registerSW }) => {
-        registerSW({ immediate: true });
-    });
-}
-
-// Register the PWA service worker
-registerSW({ immediate: true });
+import('virtual:pwa-register').then(({ registerSW }) => registerSW({ immediate: true }));
